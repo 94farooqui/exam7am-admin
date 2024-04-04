ecef01d8bb700c06aa845777daac85eb31611025
94farooqui <94.farooqui@gmail.com>
2024-04-05 02:02:12 +0400

    ready ready2

--- client/src/apiHelper/assessment.helper.js
+++ client/src/apiHelper/assessment.helper.js
@@ -1,48 +1,48 @@
-import axios from 'axios'
-const serverURL = 'http://localhost:2000'
+import axios from "axios";
+const serverURL = "https://exam7am-admin-2.onrender.com/";
 
 export const getAllAssessment = async () => {
-    
-    const response = await axios.get(`${serverURL}/api/assessments`)
+  const response = await axios.get(`${serverURL}/api/assessments`);
 
-    if(response.status === 200){
-        //console.log(response.data)
-        return response.data
-    }
+  if (response.status === 200) {
+    //console.log(response.data)
+    return response.data;
+  }
 
-    if(response.status === 401){
-        return response.error
-    }
-}
+  if (response.status === 401) {
+    return response.error;
+  }
+};
 
 export const getAssessmentDetails = async (id) => {
-    console.log(id)
-    console.log("Fetching assessment")
-    const response = await axios.get(`${serverURL}/api/assessments/${id}`)
+  console.log(id);
+  console.log("Fetching assessment");
+  const response = await axios.get(`${serverURL}/api/assessments/${id}`);
 
-    if(response.status === 200){
-        console.log(response.data)
-        return response.data
-    }
+  if (response.status === 200) {
+    console.log(response.data);
+    return response.data;
+  }
 
-    return response
-}
+  return response;
+};
 
 export const createNewAssessment = async (assessment) => {
-    console.log(assessment)
-    const response = await axios.post(`${serverURL}/api/assessments`,assessment)
+  console.log(assessment);
+  const response = await axios.post(`${serverURL}/api/assessments`, assessment);
 
-    if(response.status === 200){
-        return true
-    }
-    else return false
-}
+  if (response.status === 200) {
+    return true;
+  } else return false;
+};
 
-export const createNewQuestion = async ({id,question}) => {
-    console.log(id,question)
-    const response = await axios.post(`${serverURL}/api/assessments/${id}`,question)
-    if(response.status === 201){
-        return true
-    }
-    else return false
-}
+export const createNewQuestion = async ({ id, question }) => {
+  console.log(id, question);
+  const response = await axios.post(
+    `${serverURL}/api/assessments/${id}`,
+    question
+  );
+  if (response.status === 201) {
+    return true;
+  } else return false;
+};
